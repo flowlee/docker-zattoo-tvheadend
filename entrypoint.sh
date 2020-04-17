0ed819cd0d97a03ad9a9d3f8d2cdf66aaddd7db3
flowlee <flowlee@gmx.net>
2020-04-17 23:19:30 +0200

    update directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,12 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 # Fix permissions
-if [ "$(stat -c %u:%g /tvh-data)" != "10710:10710" ]; then
-    echo "--- /tvh-data does not belong to tvheadend, fix permissions";
-    chown -R tvheadend:tvheadend /tvh-data;
+if [ "$(stat -c %u:%g /config)" != "10710:10710" ]; then
+    echo "--- /config does not belong to tvheadend, fix permissions";
+    chown -R tvheadend:tvheadend /config;
 fi
 
-if [ ! -d /tvh-data/conf/accesscontrol ]; then
+if [ ! -d /config/conf/accesscontrol ]; then
     echo "--- No user account set up, bootstrapping by starting with -C flag"
     set -- "$@" "-C"
 fi
